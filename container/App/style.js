1d3648c5f540e19c4342f2ba4e1f4a321278e4df
Anmol Varma <anmol_varma@github.com>
2019-02-17 23:50:30 +0530

    Different css for androind and ios

--- container/App/style.js
+++ container/App/style.js
@@ -1,11 +1,17 @@
+import { Platform } from 'react-native';
+
 export default {
-    container: {
-      flex: 1,
-      backgroundColor: '#F5FCFF',
-      marginTop: 55,
-      paddingBottom: 55
-    },
-    body: {
-      flex: 1
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    ...Platform.select({
+      ios: {
+        marginTop: 55,
+        paddingBottom: 55
+      }
+    })
+  },
+  body: {
+    flex: 1
   }
+}
